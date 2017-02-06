9aeb0db5ca59ad6c9a0ff206d9967b87512fc0d2
lanefaison2 <faisonusmc@gmail.com>
2017-02-06 14:57:06 -0700

    CSS added to email owner buttons, email works

--- public/assets/scripts/find.js
+++ public/assets/scripts/find.js
@@ -3,11 +3,11 @@
 function initMap() {
   var broncos = {lat: 39.743993, lng: -105.021949};
   var locations = [
-    ['$20', 39.743981,-105.029849, 'info','A', 'example@yahoo.com'],
-    ['$30', 39.743,-105.02, 'info','B'],
-    ['$5', 39.7437,-105.03284, 'info','C'],
-    ['$12', 39.742,-105.0116, 'info','D'],
-    ['$test', 39.751,-105.0214, 'test_info', 'test E']
+    ['$20', 39.743981,-105.029849, 'info','A', 'example1@yahoo.com','Broncos vs. Patriots'],
+    ['$30', 39.743,-105.02, 'info','B','example2@yahoo.com','Broncos vs. Patriots'],
+    ['$5', 39.7437,-105.03284, 'info','C','example3@yahoo.com','Broncos vs. Chiefs'],
+    ['$12', 39.742,-105.0116, 'info','D','example4@yahoo.com','Broncos vs. Patriots'],
+    ['$test', 39.751,-105.0214, 'test_info', 'test E','example5@yahoo.com','Broncos vs. Chiefs']
   ];
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -17,9 +17,10 @@ function initMap() {
 
   var infowindow = new google.maps.InfoWindow;
 
-  var marker, i;
+  var marker;
 
-  for (i=0; i<locations.length;i++) {
+  for (let i=0; i<locations.length;i++) {
+    // if (locations[i][6] == )
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[i][1],locations[i][2]),
         map: map
@@ -42,12 +43,13 @@ function initMap() {
     var $section = $('<section>').text(locations[i][4] + ": " + locations[i][0] + ", " + locations[i][3]);
 
     var $email = $('<input class="emailOwner" type="button" value="Email owner" />',{value: locations[i][5]});
+    $section.append($email);
+    $('.location_list').append($section);
 
-    $('.location_list').append($section).append($email);
-
-    $('.emailOwner').on('click', function(event) {
+    $email.on('click', function(event) {
       event.preventDefault();
-      var email = 'christopher.faison@me.com';
+
+      let email = locations[i][5];
       var subject = 'myDriveway: I would like to rent your spot!';
       var emailBody = 'Hello, is your spot still available?';
       window.location = 'mailto:' + email + '?subject=' + subject + '&body=' + emailBody;
